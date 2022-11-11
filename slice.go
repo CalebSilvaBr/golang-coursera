990110dfb634a81ca9996034382b5d675cfcf622
Caleb Silva <106850025+CalebSilvaBr@users.noreply.github.com>
2022-11-11 18:52:07 -0300

    Update slice.go
    Added the option to request the user to input integers in case they type letters (which ended up counting as extra 0's in the slice).

--- slice.go
+++ slice.go
@@ -9,25 +9,30 @@ import (
 )
 
 func main() {
+
+	const alpha = "abcdefghijklmnopqrstuvwyz"
 	intList := make([]int, 0, 3)
 
 	var scanValue string
 	for {
 		fmt.Print("Enter an integer", ": ")
 		_, err := fmt.Scan(&scanValue)
-		exitCode := strings.ToLower(scanValue)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(0)
 		}
-		if exitCode == "x" {
+		exitCode := strings.ToLower(scanValue)
+
+		if strings.ContainsAny(alpha, exitCode) {
+			fmt.Println("Please, enter an integer!")
+		} else if exitCode == "x" {
 			fmt.Println("Exiting...")
 			os.Exit(0)
+		} else {
+			parseIntScanValue, _ := strconv.Atoi(scanValue)
+			intList = append(intList, parseIntScanValue)
+			sort.Ints(intList)
+			fmt.Println(intList)
 		}
-
-		parseIntScanValue, err := strconv.Atoi(scanValue)
-		intList = append(intList, parseIntScanValue)
-		sort.Ints(intList)
-		fmt.Println(intList)
 	}
 }
